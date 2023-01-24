c7e0b1a0a08197a4a923a216b03dbcc36809590e
Durmuş <demirtasdurmus@gmail.com>
2023-01-24 23:02:12 +0300

    ADD admin route

--- apps/api/src/app/modules/user/user.controller.ts
+++ apps/api/src/app/modules/user/user.controller.ts
@@ -4,20 +4,25 @@ import { RoleGuard } from '../auth/guards/roles.guard';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
 import { TSuccessResponse } from '@nx-monorepo/nest';
-import { UserRoles } from '@nx-monorepo/backend/core';
+import { User, UserRoles } from '@nx-monorepo/backend/core';
 import { Request } from 'express';
 
 @Controller('users')
 export class UserController {
     constructor(private readonly userService: UserService) {}
 
+    @UseGuards(RoleGuard(UserRoles.ADMIN))
+    @UseGuards(JwtAuthGuard)
+    @Get()
+    getAllUsers(): Promise<User[]> {
+        return this.userService.getAll();
+    }
+
     @Post('create')
     createUser(@Body() createUserDto: CreateUserDto): Promise<TSuccessResponse> {
         return this.userService.create(createUserDto);
     }
 
-    @UseGuards(RoleGuard(UserRoles.ADMIN))
-    @UseGuards(RoleGuard(UserRoles.USER))
     @UseGuards(JwtAuthGuard)
     @Get('profile')
     getProfile(@Req() req: Request) {
